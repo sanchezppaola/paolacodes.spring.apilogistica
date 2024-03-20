6361ea108c6a6906ebe111a289eba61b0b979026
sanchezppaola <paola@itout.com.br>
2024-03-19 21:26:48 -0300

    commit de projeto inicial

---
+++ src/test/java/com/paolacodes/spring/apilogistica/ApiLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paolacodes.spring.apilogistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
